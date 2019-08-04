d1dcde9be10b986588c2713a6e460cddf562f385
Rahsun McAfee <rahsun@rahsunmcafee.com>
2019-08-03 20:43:02 -0400

    stubbing

--- lib/repo_locker.ex
+++ lib/repo_locker.ex
@@ -1,4 +1,5 @@
 defmodule RepoLocker do
   @moduledoc """
+  This module makes calls to secure or lock up a repository.
   """
 end
--- test/repo_locker_test.exs
+++ test/repo_locker_test.exs
@@ -1,3 +1,8 @@
 defmodule RepoLockerTest do
   use ExUnit.Case
+
+  describe "lock" do
+    test "sets permissions on branch" do
+    end
+  end
 end
